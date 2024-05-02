e0bd515a50f6ed40fc0f73af5b153938066b7f38
Jimin76 <jimincho0276@gmail.com>
2024-05-02 18:38:21 +0700

    ㅇㅇ
    ㅇ

--- src 오후 1.17.10/main/java/com/example/group4_icms/HelloApplication.java
+++ src 오후 1.17.10/main/java/com/example/group4_icms/HelloApplication.java
@@ -11,12 +11,18 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+<<<<<<< Updated upstream:src 오후 1.17.10/main/java/com/example/group4_icms/HelloApplication.java
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/group4_icms/fxml/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1440, 1024);
 
         scene.getStylesheets().add(getClass().getResource("/com/example/group4_icms/css/admin_home.css").toExternalForm());
 
         stage.setTitle("ICMS Application");
+=======
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Claim Data!");
+>>>>>>> Stashed changes:src/main/java/com/example/group4_icms/HelloApplication.java
         stage.setScene(scene);
         stage.show();
     }
--- src 오후 1.17.10/main/java/module-info.java
+++ src 오후 1.17.10/main/java/module-info.java
@@ -6,6 +6,6 @@ module com.example.group4_icms {
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
 
-    opens com.example.group4_icms to javafx.fxml;
+    opens com.example.group4_icms.Functions to javafx.fxml, javafx.base;
     exports com.example.group4_icms;
 }
