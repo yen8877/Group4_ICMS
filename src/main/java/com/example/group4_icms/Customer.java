286d8be51a6e7889eb5ac6d666d145a817ca09b0
Jimin76 <118871123+Jimin76@users.noreply.github.com>
2024-05-13 16:36:03 +0700

    커스터머 테이블 업데이트

--- src/main/java/com/example/group4_icms/Customer.java
+++ src/main/java/com/example/group4_icms/Customer.java
@@ -4,48 +4,127 @@ import java.time.LocalDate;
 
 public class Customer {
     private String cId;
-    private String phoneNumber;
-    private String address;
-    private String email;
-    private LocalDate expirationDate;
-    private LocalDate effectiveDate;
-    private String insuranceCard;
     private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String role;
+    private LocalDate effectiveDate;
+    private LocalDate expirationDate;
+    private String insuranceCard;
     private String policyOwnerName;
 
-    public Customer(String cId, String phoneNumber, String address, String email,
-                    LocalDate expirationDate, LocalDate effectiveDate, String insuranceCard,
-                    String fullName, String policyOwnerName) {
+    // Constructor
+    public Customer(String cId, String fullName, String phoneNumber, String email, String address,
+                    String role, String insuranceCard, String policyOwnerName,
+                    LocalDate effectiveDate, LocalDate expirationDate) {
         this.cId = cId;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.email = email;
-        this.expirationDate = expirationDate;
-        this.effectiveDate = effectiveDate;
-        this.insuranceCard = insuranceCard;
         this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.role = role;
+        this.insuranceCard = insuranceCard;
+        this.policyOwnerName = policyOwnerName;
+        this.effectiveDate = effectiveDate;
+        this.expirationDate = expirationDate;
+    }
+
+    // Getters and Setters
+    public String getCId() {
+        return cId;
+    }
+
+    public void setCId(String cId) {
+        this.cId = cId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getInsuranceCard() {
+        return insuranceCard;
+    }
+
+    public void setInsuranceCard(String insuranceCard) {
+        this.insuranceCard = insuranceCard;
+    }
+
+    public String getPolicyOwnerName() {
+        return policyOwnerName;
+    }
+
+    public void setPolicyOwnerName(String policyOwnerName) {
         this.policyOwnerName = policyOwnerName;
     }
 
-    // Getters
-    public String getCId() { return cId; }
-    public String getPhoneNumber() { return phoneNumber; }
-    public String getAddress() { return address; }
-    public String getEmail() { return email; }
-    public LocalDate getExpirationDate() { return expirationDate; }
-    public LocalDate getEffectiveDate() { return effectiveDate; }
-    public String getInsuranceCard() { return insuranceCard; }
-    public String getFullName() { return fullName; }
-    public String getPolicyOwnerName() { return policyOwnerName; }
-
-    // Setters
-    public void setCId(String cId) { this.cId = cId; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-    public void setAddress(String address) { this.address = address; }
-    public void setEmail(String email) { this.email = email; }
-    public void setExpirationDate(LocalDate expirationDate) { this.expirationDate = expirationDate; }
-    public void setEffectiveDate(LocalDate effectiveDate) { this.effectiveDate = effectiveDate; }
-    public void setInsuranceCard(String insuranceCard) { this.insuranceCard = insuranceCard; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
-    public void setPolicyOwnerName(String policyOwnerName) { this.policyOwnerName = policyOwnerName; }
+    // toString() method to assist in debugging and logging
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "cId='" + cId + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", role='" + role + '\'' +
+                ", insuranceCard='" + insuranceCard + '\'' +
+                ", policyOwnerName='" + policyOwnerName + '\'' +
+                ", effectiveDate=" + effectiveDate +
+                ", expirationDate=" + expirationDate +
+                '}';
+    }
 }
